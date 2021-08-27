ff9fa85610be3aaf869b758c6c2112a3b6fdc592
PranavBharadwaj-1328 <bharadwaj.pranav1328@gmail.com>
2021-08-27 13:43:30 +0530

    added authorization route

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def generateToken():
     con = mysql.connector.connect(host='remotemysql.com',database='cVLw2NAjNX',user='cVLw2NAjNX',password='7I3RP65o9I')
     cursor = con.cursor()
-    query = "Insert into tokens (client_id, token) values (%s,%s)"
+    query = "insert into tokens (client_id, token) values (%s,%s)"
     k = random.randint(0,199999)
     s = str(k)
     token = hashlib.sha224(s.encode()).hexdigest()
@@ -20,11 +20,12 @@ def generateToken():
     con.close()
     print(token)
     return token
+
 @app.route('/createclient',methods=['GET'])
 def createClient():
     con = mysql.connector.connect(host='remotemysql.com',database='cVLw2NAjNX',user='cVLw2NAjNX',password='7I3RP65o9I')
     cursor = con.cursor()
-    query = "Insert into oAuth (emp_id, client_id) values (%s,%s)"
+    query = "insert into oAuth (emp_id, client_id) values (%s,%s)"
     k = random.randint(0,19999)
     s = str(k)
     client = hashlib.sha256(s.encode()).hexdigest()
@@ -33,7 +34,20 @@ def createClient():
     con.commit()
     cursor.close()
     con.close()
-    return client    
+    return client
+
+@app.route('/authorize',methods=['GET'])
+def authorizeToken():
+    con = mysql.connector.connect(host='remotemysql.com',database='cVLw2NAjNX',user='cVLw2NAjNX',password='7I3RP65o9I')
+    cursor = con.cursor()
+    query = "select * from tokens where token = %s"
+    data=(request.args.get('token'),)
+    cursor.execute(query, data)
+    rows = cursor.fetchall()
+    if(len(rows) != 0):
+        return "valid token"
+    else:
+        return "invalid token"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8090,debug=False)

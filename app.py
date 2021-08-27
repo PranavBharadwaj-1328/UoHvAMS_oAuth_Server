e629cee15b103a17623252d4569b2aa0ebd7dd3e
Pranav <39663257+PranavBharadwaj-1328@users.noreply.github.com>
2021-08-27 11:59:47 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import *
+import random
+import hashlib
+import mysql.connector
+
+app = Flask(__name__)
+con = mysql.connector.connect(host='remotemysql.com',database='cVLw2NAjNX',user='cVLw2NAjNX',password='7I3RP65o9I')
+
+@app.route('/generatetoken',methods=['GET'])
+def generateToken():
+    cursor = con.cursor()
+    query = "Insert into tokens (client_id, token) values (%s,%s)"
+    k = random.randint(0,199999)
+    s = str(k)
+    token = hashlib.sha224(s.encode()).hexdigest()
+    data = (request.args.get('id'),token)
+    cursor.execute(query, data)
+    con.commit()
+    cursor.close()
+    print(token)
+    return token
+@app.route('/createclient',methods=['GET'])
+def createClient():
+    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8090,debug=False)
